b800dfa9d338499e01d0ae1184e8ff2806dd88b3
Emizun <emiliojzv@hotmail.com>
2021-11-18 15:54:30 -0600

    LlamandoForms

--- Form3.vb
+++ Form3.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form3
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        Form4.Show()
     End Sub
 
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -9,5 +9,9 @@
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
 
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Form7.Show()
     End Sub
 End Class
