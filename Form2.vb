bd4bededd1119ff68b65fd97e1bb047ef88f97d7
ZUMBADO HERRERA <gabriel.zumbado1@ulatina.net>
2021-11-17 22:22:04 -0600

    ActulizacionG

--- Form2.vb
+++ Form2.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Form2
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
-
         MsgBox("El ingreso de datos fue realizado con exito")
 
 
     End Sub
+
 End Class
