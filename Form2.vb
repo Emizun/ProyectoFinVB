2df3073f7c36057b870f9443e94479c492f80c64
ZUMBADO HERRERA <gabriel.zumbado1@ulatina.net>
2021-11-23 20:29:03 -0600

    ActulizacionG

--- Form2.vb
+++ Form2.vb
@@ -33,28 +33,35 @@ Public Class Form2
 
     Private Sub btCrearNewUser_Click(sender As Object, e As EventArgs) Handles btCrearNewUser.Click
 
+        If txtBoxNombre.Text <> " " And txtBoxApellidos.Text <> " " And txtBoxNumID.Text <> " " And txtBoxEmail.Text <> " " And txtBoxTelefono.Text <> " " And txtBoxContrasena.Text <> " " And txtBoxConfirContra.Text <> " " Then
 
-        Dim InsertSql = "INSERT INTO " & "RegistroUS" & "(Nombre, Apellidos, NumID, Email, Telefono, Contrasena, ConfirContra)" & "VALUES" & "(@Nombre, @Apellidos, @NumID, @Email, @Telefono, @Contrasena, @ConfirContra)"
+            Dim InsertSql As String
 
-        Using con As New SqlConnection(stringConnection)
 
-            Dim cmd As New SqlCommand(InsertSql, con)
+            InsertSql = "INSERT INTO RegistroUS (Nombre, Apellidos, NumID, Email, Telefono, Contrasena, ConfirContra) VALUES (@Nombre, @Apellidos, @NumID, @Email, @Telefono, @Contrasena, @ConfirContra)"
 
-            cmd.Parameters.AddWithValue("@Nombre", txtBoxNombre)
-            cmd.Parameters.AddWithValue("@Apellidos", txtBoxApellidos)
-            cmd.Parameters.AddWithValue("@NumID", txtBoxNumID)
-            cmd.Parameters.AddWithValue("@Email", txtBoxEmail)
-            cmd.Parameters.AddWithValue("@Telefono", txtBoxTelefono)
-            cmd.Parameters.AddWithValue("@Contrasena", txtBoxContrasena)
-            cmd.Parameters.AddWithValue("@ConfirContra", txtBoxConfirContra)
+            Using con As New SqlConnection(stringConnection)
 
-            'con.Open()
-            'Dim t As Integer = CInt(cmd.ExecuteScalar())
-            'con.Close()
+                Dim cmd As New SqlCommand(InsertSql, con)
 
-        End Using
-        Call CargarGrid()
-        MsgBox("Ha creado un usuario satisfactoriamente")
+                cmd.Parameters.AddWithValue("@Nombre", txtBoxNombre)
+                cmd.Parameters.AddWithValue("@Apellidos", txtBoxApellidos)
+                cmd.Parameters.AddWithValue("@NumID", txtBoxNumID)
+                cmd.Parameters.AddWithValue("@Email", txtBoxEmail)
+                cmd.Parameters.AddWithValue("@Telefono", txtBoxTelefono)
+                cmd.Parameters.AddWithValue("@Contrasena", txtBoxContrasena)
+                cmd.Parameters.AddWithValue("@ConfirContra", txtBoxConfirContra)
+
+                'con.Open()
+                'Dim t As Integer = CInt(cmd.ExecuteScalar())
+                'con.Close()
+
+            End Using
+            Call CargarGrid()
+            MsgBox("Ha creado un usuario satisfactoriamente")
+
+        End If
+
 
     End Sub
 End Class
