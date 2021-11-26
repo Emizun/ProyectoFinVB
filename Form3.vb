50f46ec9ce2f1188cd627d28067e4957020a53e1
Emizun <emiliojzv@hotmail.com>
2021-11-25 19:34:04 -0600

    BotonSalirE

--- Form3.vb
+++ Form3.vb
@@ -8,7 +8,8 @@
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
-
+        Me.Close()
+        Form1.Show()
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs)
@@ -25,6 +26,16 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Form7.Show()
+
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        Form6.Show()
+
+    End Sub
+
+    Private Sub btIngresar_Click(sender As Object, e As EventArgs) Handles btIngresar.Click
+        Form8.Show()
 
     End Sub
 End Class
