b749f11792780d43b87cce4866691d241ddbfee3
Emizun <emiliojzv@hotmail.com>
2021-12-02 14:02:38 -0600

    Form1Listo

--- Form1.vb
+++ Form1.vb
@@ -3,9 +3,9 @@ Imports System.Data.SqlClient
 
 Public Class Form1
 
-    'Dim conexion As New SqlConnection
-    ' Dim adaptador As New SqlDataAdapter
-    'Dim datos As New DataSet
+    Dim conexion As New SqlConnection
+    Dim adaptador As New SqlDataAdapter
+    Dim datos As New DataSet
 
 
 
@@ -14,35 +14,37 @@ Public Class Form1
 
     End Sub
 
-    ' Dim usuario As String
-    'Dim contra As String
+    Dim usuario As String
+    Dim contra As String
 
     Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btIngresar.Click
 
-        ' usuario = txtUsuario.Text
-        'contra = txtContraseña.Text
-        'Dim consulta As String
-        'Dim lista As Byte
+        Dim conexion As SqlConnection = New SqlConnection("Data Source = DESKTOP-FRPTMCQ; initial catalog = ProyectoFinal; Integrated security = True")
+
+        usuario = txtUsuario.Text
+        contra = txtContraseña.Text
+        Dim consulta As String
+        Dim lista As Byte
 
 
-        'If txtUsuario.Text <> "" And txtContraseña.Text <> "" Then
-        'consulta = "SELECT * FROM RegistroUS WHERE Nombre ='" & txtUsuario.Text & "' and Contrasena = '" & txtContraseña.Text & "'"
-        'adaptador = New SqlDataAdapter(consulta, conexion)
-        'datos = New DataSet
-        'adaptador.Fill(datos, "RegistroUS")
-        'lista = datos.Tables("RegistrosUS").Rows.Count
+        If txtUsuario.Text <> "" And txtContraseña.Text <> "" Then
+            consulta = "SELECT * FROM RegistroUS WHERE Nombre ='" & txtUsuario.Text & "' and Contrasena = '" & txtContraseña.Text & "'"
+            adaptador = New SqlDataAdapter(consulta, conexion)
+            datos = New DataSet
+            adaptador.Fill(datos, "ProyectoFinal")
+            lista = datos.Tables("ProyectoFinal").Rows.Count
 
 
 
-        'End If
+        End If
 
-        'If lista <> 0 Then
-        'MsgBox("El usuario se hizo correctamente")
-        'Form3.Show()
-        'Else
-        'MsgBox("El usuario no es valido, Intente nuevamente")
+        If lista <> 0 Then
+            MsgBox("El usuario se hizo correctamente")
+            Form3.Show()
+        Else
+            MsgBox("El usuario no es valido, Intente nuevamente")
 
-        'End If
+        End If
 
 
 
@@ -69,6 +71,10 @@ Public Class Form1
     End Sub
 
     Private Sub txtContraseña_TextChanged(sender As Object, e As EventArgs) Handles txtContraseña.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
